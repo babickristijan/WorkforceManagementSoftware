b194a907425ad71553ba26bafc5eed2fcf388bfe
Sebastian <seba7964@gmail.com>
2018-10-24 00:34:34 +0200

    Spojena baza, isto ovako cemo se spajati na njihovu bazu

--- WorkforceManagementSoftware/Site.Master.cs
+++ WorkforceManagementSoftware/Site.Master.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -11,6 +13,24 @@ namespace WorkforceManagementSoftware
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            string connStr = ConfigurationManager.ConnectionStrings["myConnectionString"].ConnectionString;
+            if (ConfigurationManager.ConnectionStrings["myConnectionString"] != null)
+                Response.Write("postoji koonekcija");
+            using (SqlConnection connection = new SqlConnection(connStr))
+            using (SqlCommand command = new SqlCommand("SELECT TOP (1000) [ID] FROM[metrisco_apartmentsgalli].[metrisco_seba].[Table_1] where ID = '5'", connection))
+            {
+                connection.Open();
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Response.Write(reader["ID"].ToString());
+                        Response.Write("test");
+                    
+                    }
+                }
+            }
+
 
         }
     }
