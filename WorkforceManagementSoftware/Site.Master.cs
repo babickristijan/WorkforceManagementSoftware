f5ab5b279a4843217d26ea7cb3da87d3c08bfac2
Kristijan Babić <kristijan.babic1@gmail.com>
2018-11-11 15:30:06 +0100

    Login napravljen (hasiranje treba sredit jos ) Za prijavu koristite  username: test   pass: test

--- WorkforceManagementSoftware/Site.Master.cs
+++ WorkforceManagementSoftware/Site.Master.cs
@@ -9,7 +9,7 @@ using System.Web.Mvc;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-
+using System.Web.Security;
 
 namespace WorkforceManagementSoftware
 {
@@ -23,7 +23,10 @@ namespace WorkforceManagementSoftware
         protected void Page_Load(object sender, EventArgs e)
         {
 
-           
+            if (!this.Page.User.Identity.IsAuthenticated)
+            {
+                FormsAuthentication.RedirectToLoginPage();
+            }
         }
     }
 }
--- WorkforceManagementSoftware/loginpage.aspx.designer.cs
+++ WorkforceManagementSoftware/loginpage.aspx.designer.cs
@@ -10,33 +10,24 @@
 namespace WorkforceManagementSoftware {
     
     
-    public partial class SiteMaster {
+    public partial class loginpage {
         
         /// <summary>
-        /// LoginName1 control.
+        /// form1 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.LoginName LoginName1;
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
-        /// LoginStatus1 control.
+        /// Login1 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.LoginStatus LoginStatus1;
-        
-        /// <summary>
-        /// MainContent control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder MainContent;
+        protected global::System.Web.UI.WebControls.Login Login1;
     }
 }
