57b1d751a2f6c5583d3036e4a00e67c616df3b29
Sebastian <seba7964@gmail.com>
2018-11-15 01:31:38 +0100

    implementacija hashiranje passworda

--- WorkforceManagementSoftware/loginpage.aspx.cs
+++ WorkforceManagementSoftware/loginpage.aspx.cs
@@ -11,22 +11,52 @@ using System.Configuration;
 using System.Data.SqlClient;
 using System.Web.Security;
 using System.Security.Cryptography;
+using System.Text;
 
 namespace WorkforceManagementSoftware
 {
     public partial class loginpage : System.Web.UI.Page
     {
-            protected void ValidateUser(object sender, EventArgs e)
+
+        public string Hash(string input)
+        {
+            using (SHA256 hasher = SHA256.Create())
             {
-                int userId = 0;
-                string constr = ConfigurationManager.ConnectionStrings["myConnectionString"].ConnectionString;
+                // Convert the input string to a byte array and compute the hash.
+                byte[] data = hasher.ComputeHash(Encoding.Unicode.GetBytes(input));
+
+                // Create a new Stringbuilder to collect the bytes
+                // and create a string.
+                StringBuilder sBuilder = new StringBuilder();
+
+                // Loop through each byte of the hashed data 
+                // and format each one as a hexadecimal string.
+                for (int i = 0; i < data.Length; i++)
+                {
+                    sBuilder.Append(data[i].ToString("X2"));
+                }
+
+                // Return the hexadecimal string.
+                return sBuilder.ToString();
+            }
+        }
+
+
+        protected void ValidateUser(object sender, EventArgs e)
+            {
+            int userId = 0;
+            var passwordIzBaze = "";
+            var hashiraniPassword = "";
+            string constr = ConfigurationManager.ConnectionStrings["myConnectionString"].ConnectionString;
                 using (SqlConnection con = new SqlConnection(constr))
                 {
                     using (SqlCommand cmd = new SqlCommand("Validate_User"))
                     {
-                        cmd.CommandType = CommandType.StoredProcedure;
-                        cmd.Parameters.AddWithValue("@Username", Login1.UserName);
-                         cmd.Parameters.AddWithValue("@Password", Login1.Password);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@Username", Login1.UserName);
+                    passwordIzBaze = Login1.Password.ToString();
+                    hashiraniPassword = Hash(hashiraniPassword);
+                    cmd.Parameters.AddWithValue("@Password", hashiraniPassword);
                     cmd.Connection = con;
                         con.Open();
                         userId = Convert.ToInt32(cmd.ExecuteScalar());
