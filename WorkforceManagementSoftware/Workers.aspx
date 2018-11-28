2296df75454f8212a7974afc007ce6e938b78810
Kristijan Babić <kristijan.babic1@gmail.com>
2018-11-28 20:09:55 +0100

    Suma sati radnika (incompletely)

--- WorkforceManagementSoftware/Workers.aspx
+++ WorkforceManagementSoftware/Workers.aspx
@@ -1,36 +1,14 @@
 ﻿<%@ Page Title="Workers" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Workers.aspx.cs" Inherits="WorkforceManagementSoftware.Workers" %>
 
 <asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
-    <style>
-        table, td, tr {
-            border: 1px solid #ddd;
-            text-align: left;
-        }
-
-        table {
-            border-collapse: collapse;
-            width: 100%;
-        }
-
-        tr, td {
-            padding: 15px;
-        }
-
-        .btn-primary {
-            float: right
-        }
-
-        .tableHeader {
-            background-color: lightgray;
-        }
-    </style>
-    <h2><%: Title %></h2>
+    <link href="Scripts/Workers.css" rel="stylesheet">
+    <h2><%:Title%></h2>
     <div class="wrap-padding" id='wrap'>
         <div id="employee-wrapper">
-            <asp:Button ID="addANewWorker" runat="server"  Text="Add a New Worker" CssClass="btn btn-primary" PostBackUrl="~/AddWorker.aspx" />
-           
+            <asp:Button ID="addANewWorker" runat="server" Text="Add a New Worker" CssClass="btn btn-primary" PostBackUrl="~/AddWorker.aspx" />
+
         </div>
-        <table border="1" style="margin: 2px;">
+        <table border="1" style="margin: 2px;" id="TableWorkers">
             <tr class="tableHeader">
                 <td>First Name</td>
                 <td>Last Name</td>
@@ -48,7 +26,7 @@
                         <td><%# Eval("FirstName") %></td>
                         <td><%# Eval("LastName") %></td>
                         <td><%# Eval("VacationDayLeft") %></td>
-                        <td>NULL</td>
+                        <td ID="test"></td>
                     </tr>
 
                 </ItemTemplate>
@@ -57,7 +35,7 @@
                 ID="getUsers" runat="server" SelectCommand="SELECT TOP (1000) [id], [title], [Parentid], [Email], [FirstName], [LastName], [VacationDayLeft] FROM [unipuhrhost25com_workforcemanagementsoftware].[dbo].[ResourcesChild]"></asp:SqlDataSource>
 
         </table>
-        
+
 
     </div>
 
--- WorkforceManagementSoftware/Workers.aspx.cs
+++ WorkforceManagementSoftware/Workers.aspx.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -7,11 +10,79 @@ using System.Web.UI.WebControls;
 
 namespace WorkforceManagementSoftware
 {
-    public partial class Workers: Page
+    public class EventKiki
     {
-        protected void Page_Load(object sender, EventArgs e)
+        public string ID_res;
+        public string Title;
+        public string ParentId;
+        public string Email;
+        public string FirstName;
+        public string LastName;
+        public string VacationDayLeft;
+        public string ID_event;
+        public string Res_ID;
+        public string Start;
+        public string End;
+        public string ID_smjene;
+        public string smjena_ID;
+        public string naziv_smjene;
+        public string SmjenaSati;
+        public string sumaSati;
+    }
+
+    public partial class Workers : Page
+    {
+
+        public void Page_Load(object sender, EventArgs e)
         {
-           
+            GetSumOfHour();
+        }
+
+        public string GetSumOfHour()
+        {
+            List<EventKiki> events = new List<EventKiki>();
+            Dictionary<List<EventKiki>, List<EventKiki>> myLists = new Dictionary<List<EventKiki>, List<EventKiki>>();
+            string connStr = ConfigurationManager.ConnectionStrings["myConnectionString"].ConnectionString + "MultipleActiveResultSets=true";
+            using (SqlConnection connection = new SqlConnection(connStr))
+            using (SqlCommand command = new SqlCommand("SELECT TOP (1000) [unipuhrhost25com_workforcemanagementsoftware].[dbo].[ResourcesChild].[id], [title], [Parentid], [Email], [FirstName], [LastName], [VacationDayLeft], [unipuhrhost25com_workforcemanagementsoftware].[dbo].[Events].[ID],[ResourceId],[Start],[End],[id_smjene],[unipuhrhost25com_workforcemanagementsoftware].[dbo].[Smjene].[id],[naziv],[SmjenaSati], DATEDIFF(day,[unipuhrhost25com_workforcemanagementsoftware].[dbo].[Events].[Start], [unipuhrhost25com_workforcemanagementsoftware].[dbo].[Events].[End]) * SmjenaSati as Suma FROM[unipuhrhost25com_workforcemanagementsoftware].[dbo].[ResourcesChild] JOIN[unipuhrhost25com_workforcemanagementsoftware].[dbo].[Events] ON ResourcesChild.id = Events.ResourceId JOIN[unipuhrhost25com_workforcemanagementsoftware].[dbo].[Smjene] ON Events.id_smjene = Smjene.id ORDER BY ResourcesChild.id", connection))
+            {
+                connection.Open();
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+
+                    while (reader.Read())
+                    { 
+                        EventKiki EventKiki = new EventKiki();
+                        EventKiki.ID_res = reader["id"].ToString();
+                        EventKiki.Title = reader["title"].ToString();
+                        EventKiki.ParentId = reader["Parentid"].ToString();
+                        EventKiki.FirstName = reader["FirstName"].ToString();
+                        EventKiki.LastName = reader["LastName"].ToString();
+                        EventKiki.VacationDayLeft = reader["VacationDayLeft"].ToString();
+                        EventKiki.ID_event = reader["ID"].ToString();
+                        EventKiki.Res_ID = reader["ResourceId"].ToString();
+                        EventKiki.Start = Convert.ToDateTime(reader["Start"]).ToString("MM/dd/yyyy");
+                        EventKiki.End = Convert.ToDateTime(reader["End"]).ToString("MM/dd/yyyy");
+                        EventKiki.ID_smjene = reader["id_smjene"].ToString();
+                        EventKiki.smjena_ID = reader["id"].ToString();
+                        EventKiki.naziv_smjene = reader["naziv"].ToString();
+                        EventKiki.SmjenaSati = reader["SmjenaSati"].ToString();
+                        EventKiki.sumaSati = reader["Suma"].ToString();
+
+                       
+                   
+
+    
+                        events.Add(EventKiki);
+                       
+                    
+                    }
+                }
+                connection.Close();
+            }
+
+            return null;
         }
     }
+
 }
